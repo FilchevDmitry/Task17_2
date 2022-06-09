368a1d4bfca5480fd46e2d5a92e3523abbe41f84
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-06-09 20:02:46 +0300

    Добавьте файлы проекта.

---
+++ Task17_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+void reverse(int num[], const int SIZE)
+{
+	for (int i = 0; i < SIZE/2; i++)
+	{
+		int temp = num[SIZE - 1 - i];
+		num[SIZE - 1 - i] = num[i];
+		num[i] = temp;
+	}
+}
+int main()
+{
+	const int SIZE = 10;
+	int num[SIZE] = { 1,2,3,4,5,6,7,8,9,10 };
+	reverse(num, SIZE);
+	for (int a : num)
+	{
+		std::cout << a << " ";
+	}
+}
